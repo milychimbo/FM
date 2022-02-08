b3ed4f138601f0d6cb01b62f42feb8a52417a95e
milychimbo <68032094+milychimbo@users.noreply.github.com>
2022-02-07 22:42:02 -0500

    Delete src directory

--- src/routes/api/membresia2.js
+++
@@ -1,9 +0,0 @@
-const {Router} =require('express');
-const { showMembresias, showMembresiaByID, showMembresiaByCedula,insertMembresia, updateMembresia, deleteMembresia } = require('../../logic/IMembresiaDAO');
-const { validationInsert, validationShow2, validationUpdate } = require('../../logic/validator/membresia');
-const router = Router();
-
-
-router.get('/:cedula', validationShow2,showMembresiaByCedula);
-
-module.exports=router;
